03dc7f183df698074f10848461c517f29447e7ab
sushil chaulagain <sushilchlgn@gmail.com>
2024-07-02 13:53:00 +0545

    added auth in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Auth from "./auth/Auth";
 import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
-    <>
+    <div className="w-11/12">
     <NavBar />
-    </>
+    <Auth />
+    </div>
   )
 }

c25e1cd2499c30287b6d7f0af199cadc7f670e83
sushil chaulagain <sushilchlgn@gmail.com>
2024-07-02 13:52:56 +0545

    added register page

---
+++ src/auth/Register.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <form action="">
+        <h1>personal detail</h1>
+        <div className="flex flex-col">
+          <input type="text" placeholder="First Name" className="" />
+          <input type="text" placeholder="Last Name" className="" />
+          <input type="number" placeholder="Mobile Number" className="" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
